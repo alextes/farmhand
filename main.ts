e5438385965fa799af195adab5a3e0fb9d02483c
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-05-26 14:34:56 +0200

    Up price caching to four hours

--- main.ts
+++ main.ts
@@ -4,13 +4,11 @@ import { makeApp } from "./server.ts";
 import * as Id from "./id.ts";
 
 const hostname = Deno.env.get("ENV") === "dev" ? "localhost" : "0.0.0.0";
-const oneHourInMs = milisFromHours(1);
-const fourHoursInMilis = milisFromHours(4);
-const idMapCache = new LRU({ capacity: 1, stdTTL: fourHoursInMilis });
+const idMapCache = new LRU({ capacity: 1, stdTTL: milisFromHours(4) });
 idMapCache.set(Id.idMapKey, M.empty());
 const app = makeApp({
   idMapCache,
-  priceCache: new LRU({ capacity: 1000, stdTTL: oneHourInMs }),
+  priceCache: new LRU({ capacity: 1000, stdTTL: milisFromHours(4) }),
   historicPriceCache: new LRU({ capacity: 100000 }),
 });
 await app.listen({ hostname, port: 8080 });
